12fa4328edf3b447c2e8d5e2ce457d51bda32414
Luca Vandro <lucavandro@gmail.com>
2025-09-23 18:00:54 +0200

    Aggiunta dipendenza @svelte-share/button e migliorata la logica di condivisione in +page.svelte

--- src/routes/qr/+page.svelte
+++ src/routes/qr/+page.svelte
@@ -1,11 +1,42 @@
 <div class="text-center">
     <img src="/app/way/tmp/qr.png" alt="">
+    {#if canShare}
+    <button on:click={share}>Condividi</button>
+    {:else}
+    <input type="text" readonly value="https://www.liceoscientificocortese.edu.it/app/way/tmp/">
+     <button on:click="{() => {navigator.clipboard.writeText('https://www.liceoscientificocortese.edu.it/app/way/tmp/'); showIns = true;}}">Copia link</button>
+        {#if showIns}
+        <ins>Link copiato negli appunti!</ins>
+        {/if}
+     {/if}
+     
 </div>
+
+
+<script>
+    let showIns = false;
+    const canShare = navigator.share !== undefined;
+    const appURL = "https://www.liceoscientificocortese.edu.it/app/way/tmp/";
+    function share() {
+    if (navigator.share) {
+      navigator.share({
+        title: "Way Cortese",
+        text: "Accedi all'app WAY Cortese",
+        url: appURL
+      });
+    } else {
+      alert("La condivisione nativa non è supportata su questo browser.");
+    }
+  }
+
+</script>
 <style>
+    button,
     img {
-        max-width: 100%;
+        width:350px;
         height: auto;
-        margin: 0 auto;
+        margin: 5px auto;
         display: block;
     }
+
 </style>
