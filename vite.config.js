87022bc6b3e135a422fd0fdc254aa9ebe40fc2eb
Luca Vandro <lucavandro@gmail.com>
2026-07-18 17:06:33 +0200

    Rimosso il file service-worker.js e aggiornato il registro del service worker in app.html per utilizzare il percorso corretto. Modificato vite.config.js per rimuovere il plugin VitePWA.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: [sveltekit(),    VitePWA({
-		injectRegister: 'inline',
-		devOptions: {
-		  enabled: true
-		}
-	  })],
+	plugins: [sveltekit()],
 });
