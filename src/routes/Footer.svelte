159c1046fba250f4a03365fb11d5172167cf2c2a
Luca Vandro <lucavandro@gmail.com>
2026-09-06 23:54:43 +0200

    feat: add persistent footer component with loading progress indicator

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,14 +1,12 @@
-<script> 
- import {isLoading} from "$lib/stores.js";
+<script>
+  import { isLoading } from "$lib/stores.js";
 </script>
 
-
-
 <footer>
+  <small>A.S. 2026/27</small>
   {#if $isLoading}
     <progress />
   {/if}
-  
 </footer>
 
 <style>
@@ -17,15 +15,20 @@
     left: 0;
     bottom: 0;
     width: 100%;
-    background-color:none;
-    padding:0;
-    margin-bottom:-5px;
+    background-color: none;
+    padding: 0;
+    margin-bottom: -5px;
   }
 
-  progress{
+  footer small {
+    text-align: center;
+    margin-bottom: 0;
+    margin: auto;
+    display: block;
+    padding: 1rem;
+  }
+
+  progress {
     margin-bottom: 0;
   }
-
-
-   
 </style>
